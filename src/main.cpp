410a01b544c480f2ea4acf8506d8a716be22e5f8
SapphireAmoeba5 <clevermail19@gmail.com>
2021-10-26 18:57:54 -0300

    Set up window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 
@@ -12,5 +13,29 @@ int main()
     }
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "2048", NULL, NULL);
-    
+    if(window == nullptr)
+    {
+        std::cout<<"Failed to create GLFW window"<<std::endl;
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glfwWindowHint(GLFW_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout<<"Failed to initialize OpenGL context/glad"<<std::endl;
+        return -1;
+    }
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
 }
